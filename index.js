fa77aa15f0f8fe1692200ac0336badfd881d0072
rlarkghd <118590348+rlarkghd@users.noreply.github.com>
2022-11-21 21:12:37 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './todolist';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
